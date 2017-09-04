b245c0d744e1e8dc0ad26654c184f63408284a7e
R. van Veen <rvanveen@servoy.com>
2017-09-04 11:16:58 +0200

    Added dataseeds for all dbs (Ok some of them are large!!!)

---
+++ jenkins-custom/data-seeds/example_data/shippers.sql
@@ -0,0 +1,9 @@
+--
+-- PostgreSQL database dump
+--
+
+TRUNCATE TABLE shippers;
+
+INSERT INTO shippers (shipperid, companyname, phone) VALUES (1, 'Speedy Express', '(503) 555-9831');
+INSERT INTO shippers (shipperid, companyname, phone) VALUES (2, 'United Package', '(503) 555-3199');
+INSERT INTO shippers (shipperid, companyname, phone) VALUES (3, 'Federal Shipping', '(503) 555-9931');
