5597ce57cf88613704ebaedc92a258248b6ef1c5
Paolo <paronne@servoy.com>
2018-03-28 15:20:22 +0200

    InitLessJS

---
+++ sampleGallery/sampleGallery.js
@@ -0,0 +1,16 @@
+/**
+ * Callback method for when solution is opened.
+ * When deeplinking into solutions, the argument part of the deeplink url will be passed in as the first argument
+ * All query parameters + the argument of the deeplink url will be passed in as the second argument
+ * For more information on deeplinking, see the chapters on the different Clients in the Deployment Guide.
+ *
+ * @param {String} arg startup argument part of the deeplink url with which the Client was started
+ * @param {Object<Array<String>>} queryParams all query parameters of the deeplink url with which the Client was started
+ *
+ * @properties={typeid:24,uuid:"0E3E1FC5-A8CA-4EE0-9782-17D5629658D1"}
+ */
+function onSolutionOpenSampleGallery(arg, queryParams) {
+	scopes.svyThemeRoller.addLESSFileDependancy("themes/theme-servoy.less");
+	//scopes.svyThemeRoller.addLESSFileDependancy("theme-custom.less");
+	scopes.svyThemeRoller.initLessJS();
+}
