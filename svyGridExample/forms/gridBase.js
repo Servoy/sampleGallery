e30425d76aa3e2c733d590acd8a37e69dde6db6f
Paolo <paronne@servoy.com>
2018-05-14 14:26:05 +0300

    Grid fixes

--- svyGridExample/forms/gridBase.js
+++ svyGridExample/forms/gridBase.js
@@ -1,4 +1,8 @@
 /**
+ * @properties={typeid:24,uuid:"1525A0FA-89B1-4778-9E13-A8586E115A70"}
+ */
+function onCellClick(foundsetIndex, columnIndex, record, event) {}
+/**
  * Perform the element default action.
  *
  * @param {JSEvent} event the event that triggered the action
